750769b7f21f3971dadf5cd886ca3cbb711db638
Jeremy <jeremyvriens1993@gmail.com>
2011-09-12 10:00:08 +0200

    GUI

--- RHC/WindowsFormsApplication1/Properties/AssemblyInfo.cs
+++ RHC/WindowsFormsApplication1/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RHC")]
+[assembly: AssemblyTitle("WindowsFormsApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RHC")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("WindowsFormsApplication1")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4e99e40d-7fbc-4f8e-95b3-f647aeb7d8f2")]
+[assembly: Guid("16ea17d1-9afa-4045-96fc-28be63f817a8")]
 
 // Version information for an assembly consists of the following four values:
 //
